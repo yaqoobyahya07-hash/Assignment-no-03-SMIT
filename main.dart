381035322f54a1b0eea8461b7d264ce8aa0cfbf5
Develop by yahya <yaqoobyahya07@gmail.com>
2026-09-02 21:11:42 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,18 @@
+//question no 01
+void main(){
+List names=["yahya,salim,azlaan,zeeman"];
+print(names);
+
+//question no 02
+List days=[];
+
+days.add("monday");
+days.add("tuesday");
+days.add("wednesday");
+days.add("thursday");
+days.add("friday");
+days.add("saturday");
+days.add("sunday");
+print(days);
+
+}
